6280e4cab70d10f4f236ccb571bf31ace15dcb0a
candyslut <kuznetsovczech@gmail.com>
2026-05-20 17:26:47 +0500

    Fix: merge Alpine and Livewire imports to fix vite build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,7 @@
 import './bootstrap';
+
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
-import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
-Alpine.start();
 
 Livewire.start();
